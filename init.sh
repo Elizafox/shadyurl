83be82c51e80c8318c6eee16354663a9f33b16fd
Elizabeth Myers <elizabeth.jennifer.myers@gmail.com>
2022-10-22 19:38:28 -0500

    Improve init script

--- init.sh
+++ init.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-sqlite3 urls.db <<EOF
+if [ ! -e urls.db]; then
+	echo "Creating database"
+	(sqlite3 urls.db <<EOF
 CREATE TABLE urls (
 	token VARCHAR UNIQUE NOT NULL,
 	url VARCHAR UNIQUE NOT NULL
 );
 EOF
+	) || exit 1
+fi
+
+if [ ! -e dh.pem ]; then
+	echo "Creating DH parameters"
+	openssl dhparam -out dh.pem 2048 || exit 1
+fi
+
+echo
+echo "Done! Don't forget to create cert.pem and key.pem."
